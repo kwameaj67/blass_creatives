88b0f98c7cb14b39ec108f7c2c4d52f579877dfb
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-03-31 15:41:16 +0000

    😭hmm

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../../assets/blass.png'
+import './Loader.css'
+
+
+export default function loader() {
+    return (
+            <img src={logo} className="app-logo" alt="blass logo"/>
+    )
+}
