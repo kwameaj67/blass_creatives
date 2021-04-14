2b099421b70ee1bbb94c737fe0a59e3f35b6d6ab
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-14 12:02:33 +0000

    change id

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <img className="logo-img" src={logo} alt="brass" style={{width:60,height:40}} loading="lazy" />
                 <ul className={showMenu ? "nav-menu active" : "nav-menu"}>
                   <li><a href="#home">Home</a></li>
-                  <li><a href="#about">About</a></li>
+                  <li><a href="#about-myself">About</a></li>
                   <li><a href="#gallery">Gallery</a></li>
                   <li><a href="#accessories">Gadgets</a></li>
                   <Link to="/contact"> <li><a href="#accessories">Reach me</a></li></Link>
