045bad5e1f1d3a732b8d2c14083a924365e2a23a
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-14 12:02:07 +0000

    add loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Gallery from './Pages/Gallery/Gallery'
 import Home from './Pages/Home/Home'
 import Contact from './Pages/Contact/Contact'
+import { Spinner } from 'evergreen-ui'
 
 
 function App() {
+  const [loading, setLoading ] = useState(true)
 
+  useEffect(() => {
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  },)
   return (
-    <div className="App">
-        <Router>
-            <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/gallery" component={Gallery}/>
-            <Route path="/contact" component={Contact}/>
-            </Switch>
-        </Router>
-    </div>
+    <>
+      {loading ? 
+       <div className="loader">
+          <Spinner size={30} color="white"/>
+       </div>
+       :
+       <div className="App">
+            <Router>
+              <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/gallery" component={Gallery}/>
+              <Route path="/contact" component={Contact}/>
+              </Switch>
+            </Router>
+        </div>
+    }
+    </>
   );
 }
 
