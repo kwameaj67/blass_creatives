ccbe8c970f7de87f03474e69f83e60e53bff8d3a
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-15 12:05:10 +0000

    fix navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,18 +6,24 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+        console.log(showMenu)
+    }
     return (
         <>
            <nav className="navbar animate__animated animate__fadeInDown animate__delay-1s" >
                 <img className="logo-img" src={logo} alt="brass" style={{width:60,height:40}} loading="lazy" />
-                <ul className={showMenu ? "nav-menu active" : "nav-menu"}>
+                <ul className={showMenu === true ? "nav-menu active" : "nav-menu"}>
                   <li><a href="#home">Home</a></li>
                   <li><a href="#about-myself">About</a></li>
                   <li><a href="#gallery">Gallery</a></li>
                   <li><a href="#accessories">Gadgets</a></li>
-                  <Link to="/contact"> <li><a href="#accessories">Reach me</a></li></Link>
+                  {/* <li><a href="#accessories"><Link to="/contact"> Reach me</Link></a></li> */}
+                  <Link to="/contact"><li><span>Reach me</span></li></Link>
                 </ul>
-                <div className={showMenu === true ? "menu-toggle is-active" :"menu-toggle"} id="mobile-menu" onClick={()=>{setShowMenu(!showMenu)}}>
+                <div className={showMenu === true ? "menu-toggle is-active" : "menu-toggle" } id="mobile-menu" onClick={toggleMenu}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     {/* <span className="bar"></span> */}
