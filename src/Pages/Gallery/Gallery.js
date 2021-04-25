a112b6f19407d25f603f751ca9c10ef9b01a3cc8
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-25 12:10:43 +0000

    add gsap

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -3,37 +3,56 @@ import './gallery.css'
 import logo from '../../assets/blass.png';
 import { Link } from 'react-router-dom';
 import gall1 from '../../assets/755bc551-4b94-4b27-a4bd-7295625995cb 2.jpg'
-import gall2 from '../../assets/ayo-ogunseinde-1L5QpkSs5IU-unsplash.jpg'
+import gall2 from '../../assets/ayo-ogunseinde-1L5QpkSs5IU-unsplash.webp'
 import gall3 from '../../assets/jd-mason-qvqMQf9mg04-unsplash.jpg'
 import gall4 from '../../assets/macclusky-gbekle-j2xe3HO8lDE-unsplash.jpg'
 import gall5 from '../../assets/eye-for-ebony-OExQjtxbIpE-unsplash.jpg'
 import gall6 from '../../assets/engin-akyurt-Np8r_VcKZzw-unsplash.jpg'
 import gall7 from '../../assets/oladimeji-odunsi-wecunh3nBrA-unsplash.jpg'
-import gall8 from '../../assets/alex-sorto--fowxtAtWW8-unsplash.jpg'
+import gall8 from '../../assets/alex-sorto--fowxtAtWW8-unsplash.webp'
 import gall9 from '../../assets/quaid-lagan-92YQRw3-__I-unsplash.jpg'
 import { TweenMax,TimelineLite,Power3} from 'gsap'
 
 const Gallery = () => {
     let app = useRef(null)
     let imagesSection1 = useRef(null)
-    // let imagesSection2 = useRef(null)
-    // let imagesSection3 = useRef(null)
-    // let imagesSection4 = useRef(null)
+    let imagesSection2 = useRef(null)
+    let imagesSection3 = useRef(null)
+    let imagesSection4 = useRef(null)
     let tl = new TimelineLite()
 
     const animation = ()=> {
         // IMAGES VAR
         const gall2 = imagesSection1.firstElementChild
         const gall3 = imagesSection1.lastElementChild
+        const gall5 = imagesSection2.firstElementChild
+        const gall9= imagesSection2.lastElementChild
+        const gall8 = imagesSection3.firstElementChild
+        const gall7= imagesSection3.lastElementChild
+        const gall1 = imagesSection4.firstElementChild
+        const gall4= imagesSection4.lastElementChild
 
         TweenMax.to(app,0,{css:{visibility:'visible'}})
-
-        tl.from(gall2, 1.2 , {y:1280, ease:Power3.easeOut})
+        tl.from(gall2, 1.2 , {y:1280, ease:Power3.easeOut})  // 2,3 
         .from(gall2.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
         .from(gall3, 1.2 , {y:1280, ease:Power3.easeOut}, .2)
         .from(gall3.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
         
-        console.log(gall2,gall3)
+        tl.from(gall5, 1.2 , {y:1280, ease:Power3.easeOut})  // 5,9
+        .from(gall5.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
+        .from(gall9, 1.2 , {y:1280, ease:Power3.easeOut}, .2)
+        .from(gall9.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
+        // console.log(gall2,gall3)
+
+        tl.from(gall8, 1.2 , {y:1280, ease:Power3.easeOut})  // 8,7
+        .from(gall8.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
+        .from(gall7, 1.2 , {y:1280, ease:Power3.easeOut}, .2)
+        .from(gall7.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
+
+        tl.from(gall1, 1.2 , {y:1280, ease:Power3.easeOut})  // 1,4
+        .from(gall1.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
+        .from(gall4, 1.2 , {y:1280, ease:Power3.easeOut}, .2)
+        .from(gall4.firstElementChild, 2, { scale:1.6, ease:Power3.easeInOut}, .2)
     }
     useEffect(() => {
         document.title = "Gallery - BlassCreatives"
@@ -61,18 +80,18 @@ const Gallery = () => {
                    <img src={gall2} alt="gall2" className="gall2" loading="lazy" /> 
                    <img src={gall3} alt="gall3" className="gall3"  loading="lazy"/> 
                 </div>
-                <div className="gallery-img " >
+                <div className="gallery-img" ref={el => imagesSection2 = el}>
                    <img src={gall5} alt="gall5" className="gall5" loading="lazy"/> 
                    <img src={gall9} alt="gall9" className="gall9" loading="lazy"/> 
                 </div>
-                <div className="gallery-img " >
+                <div className="gallery-img " ref={el => imagesSection3 = el}>
                    <img src={gall8} alt="gall8" className="gall8" loading="lazy"/> 
                    <img src={gall7} alt="gall7" className="gall7" loading="lazy"/> 
                 </div>
                 <div className="gallery-img " >
                    <img src={gall6} alt="gall6" className="gall6" loading="lazy"/> 
                 </div>
-                <div className="gallery-img " >
+                <div className="gallery-img " ref={el => imagesSection4 = el} >
                     <img src={gall1} alt="gall1" className="gall1" loading="lazy" /> 
                    <img src={gall4} alt="gall4" className="gall4" loading="lazy"/> 
                 </div>
